38367c21ea8d05b6e5110feba98289f32bde19d4
DarkAbood <abdallahalaa594@gmail.com>
2021-04-18 16:37:41 +0300

    Added Enemeys
    Added Enemeys

---
+++ Assets/Scripts/EnemyGroundMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGroundMovement : MonoBehaviour
+{
+    public float speed;
+    public float Distance;
+    private bool MovingRight = true;
+    public Transform GroundDetection;
+    public LayerMask groundLayer;
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        RaycastHit2D Groundinfo = Physics2D.Raycast(GroundDetection.position, Vector2.down, Distance,groundLayer);
+        if(Groundinfo.collider == false)
+        {
+            if (MovingRight)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                MovingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                MovingRight = true;
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float Speed;
+    public float JumpForce;
+    [Header("Ground Check")]
+    bool IsGrounded;
+    public Transform GroundCheck;
+    public float CheckRaduis;
+    public LayerMask GroundLayer;
+    [Header("Coin")]
+    [HideInInspector] public int Coins;
+    public Text CoinsText;
+    public GameObject CoinParticle;
+    [Header("Animations")]
+    public Animator anim;
+    void Update()
+    {
+        Move();
+        GroundChecker();
+        Jump();
+        Attack();
+    }
+
+    void Move()
+    {
+        //Moveing the player on the x axis
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * Speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+        //-1 1 0
+        if (x == -1)
+        {
+            anim.SetBool("IsIdle", false);
+            transform.localScale = new Vector3(-1, 1);
+        }else if (x == 1)
+        {
+            transform.localScale = new Vector3(1, 1);
+            anim.SetBool("IsIdle", false);
+        }
+        else if (x == 0)
+        {
+            anim.SetBool("IsIdle", true);
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && IsGrounded == true)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+        }
+    }
+    void GroundChecker()
+    {
+        Collider2D coll = Physics2D.OverlapCircle(GroundCheck.position, CheckRaduis, GroundLayer);
+        if(coll != null)
+        {
+            IsGrounded = true;
+        }
+        else
+        {
+            IsGrounded = false;
+        }
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            UpdateCoins(1);
+            GameObject Particle = Instantiate(CoinParticle, other.transform.position, Quaternion.identity);
+            Destroy(Particle, 0.5f);
+            Destroy(other.gameObject);
+        }
+    }
+
+    void UpdateCoins(int amount)
+    {
+        Coins += amount;
+        CoinsText.text = "Coins: " + Coins;
+    }
+    [Header("Attack")]
+    public Transform HitPos;
+    public float HitRad;
+    public LayerMask EnemyLayermask;
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Collider2D[] AttackInfo = Physics2D.OverlapCircleAll(HitPos.position, HitRad, EnemyLayermask);
+            foreach (Collider2D enemy in AttackInfo)
+            {
+                Destroy(enemy.gameObject);
+            }
+        }
+    }
+}
