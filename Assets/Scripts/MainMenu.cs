44a4c6de1d9ec2b225720e6053f96d588eb8932b
DarkAbood <abdallahalaa594@gmail.com>
2021-04-21 11:20:27 +0300

    Video #7
    added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quitting");
+    }
+}
